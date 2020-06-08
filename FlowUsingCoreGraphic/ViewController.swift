e2226d5a4f6b60342e6b47b993fc1431dd5c4aba
Pankaj Kumar <pankaj.kumar@vinsol.com>
2020-06-08 11:35:41 +0530

    add Arc using bizier path and buttons for increasing decreasing value

--- FlowUsingCoreGraphic/ViewController.swift
+++ FlowUsingCoreGraphic/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var counterView: CounterView!
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        counterLabel.text = String(counterView.counter)
     }
-
-
+    
+    @IBAction func pushButtonPressed(_ button: PushButton) {
+        if button.isAddButton {
+            counterView.counter += 1
+        } else {
+            if counterView.counter > 0 {
+                counterView.counter -= 1
+            }
+        }
+        counterLabel.text = String(counterView.counter)
+    }
+    
 }
 
